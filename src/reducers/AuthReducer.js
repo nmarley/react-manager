399da4363577b62057bf180e1f0dfb63e83e8d32
Nathan Marley <nathan.marley@gmail.com>
2018-05-16 07:49:31 -0700

    handle new account / fail case, cleanup

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -2,6 +2,7 @@ import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
   LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL,
 } from '../actions/types';
 
 const INITIAL_STATE = {
@@ -19,6 +20,8 @@ export default (state = INITIAL_STATE, action) => {
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
     case LOGIN_USER_SUCCESS:
+      return { ...state, user: action.payload };
+    case LOGIN_USER_FAIL:
       return { ...state, user: action.payload };
     default:
       return state;
