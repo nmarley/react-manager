8aaa531161f97906c7195c6c2ff24f5f80c0697a
Nathan Marley <nathan.marley@gmail.com>
2018-05-16 07:39:06 -0700

    add async reducer w/redux-thunk, user login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
@@ -16,3 +17,12 @@ export const passwordChanged = text => (
     payload: text,
   }
 );
+
+export const loginUser = ({ email, password }) => {
+  return (dispatch) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((user) => {
+        dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user });
+      });
+  };
+};
