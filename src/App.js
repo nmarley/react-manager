c83ab7ba9a0a25e6887a2985e23046fae3476f08
Nathan Marley <nathan.marley@gmail.com>
2018-05-15 18:48:18 -0700

    create basic reducer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './reducers';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Hello!
