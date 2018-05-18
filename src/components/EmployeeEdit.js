8e2d6411312ad031fc281bf5f020527b8ec54012
Nathan Marley <nathan.marley@gmail.com>
2018-05-18 16:55:28 -0700

    add edit screen and pre-populate view

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,26 +1,31 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { employeeUpdate, employeeCreate } from '../actions';
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Button } from './common';
 import EmployeeForm from './EmployeeForm';
 
-class EmployeeCreate extends Component {
+class EmployeeEdit extends Component {
+  componentWillMount() {
+    _.each(this.props.employee, (value, prop) => {
+      this.props.employeeUpdate({ prop, value });
+    });
+  }
+
   onButtonPress() {
     const { name, phone, shift } = this.props;
 
-    this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+    this.props.employeeUpdate({ name, phone, shift });
   }
 
   render() {
     return (
       <Card>
-        <EmployeeForm {...this.props} />
+        <EmployeeForm />
 
         <CardSection>
-          <Button
-            onPress={this.onButtonPress.bind(this)}
-          >
-            Create
+          <Button onPress={this.onButtonPress.bind(this)}>
+            Save Changes
           </Button>
         </CardSection>
       </Card>
@@ -30,11 +35,7 @@ class EmployeeCreate extends Component {
 
 const mapStateToProps = (state) => {
   const { name, phone, shift } = state.employeeForm;
-
   return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, {
-  employeeUpdate,
-  employeeCreate,
-})(EmployeeCreate);
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
