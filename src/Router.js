59188f9013d39527bcc97dca04ba015fa7cad041
Nathan Marley <nathan.marley@gmail.com>
2018-05-17 20:25:41 -0700

    add employee create form and nav to/from

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
+import { Actions } from 'react-native-router-flux';
 
 const RouterComponent = () => {
   return (
@@ -11,7 +13,19 @@ const RouterComponent = () => {
           <Scene key="login" component={LoginForm} title="Please Login" initial />
         </Scene>
         <Scene key="main">
-          <Scene key="employeeList" component={EmployeeList} title="Employees" />
+          <Scene
+            rightTitle="Add"
+            onRight={() => Actions.employeeCreate()}
+            key="employeeList"
+            component={EmployeeList}
+            title="Employees"
+            initial
+          />
+          <Scene
+            key="employeeCreate"
+            component={EmployeeCreate}
+            title="Create Employee"
+          />
         </Scene>
       </Scene>
     </Router>
