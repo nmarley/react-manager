5dff6fbdba75a638609844358e43ee48ed2516d9
Nathan Marley <nathan.marley@gmail.com>
2018-05-17 20:39:18 -0700

    set state in employee form reducer

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -3,12 +3,15 @@ import {
 } from '../actions/types';
 
 const INITIAL_STATE = {
+  name: '',
+  phone: '',
+  shift: '',
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMPLOYEE_UPDATE:
-      return { ...state };
+      return { ...state, [action.payload.prop]: action.payload.value };
     default:
       return state;
   }
