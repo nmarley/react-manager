ac6d1eedf19df6b76453927211d7bf5997dfc867
Nathan Marley <nathan.marley@gmail.com>
2018-05-18 14:26:20 -0700

    convert listview to flatlist

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
-import { ListView } from 'react-native';
+import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
 import { employeesFetch } from '../actions';
 import ListItem from './ListItem';
@@ -20,23 +20,18 @@ class EmployeeList extends Component {
   }
 
   createDataSource({ employees }) {
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
-
-    this.dataSource = ds.cloneWithRows(employees);
+    this.data = employees;
   }
 
-  renderRow(employee) {
-    return <ListItem employee={employee} />;
+  renderItem({ item, index }) {
+    return <ListItem employee={item} />;
   }
 
   render() {
     return (
-      <ListView
-        enableEmptySections
-        dataSource={this.dataSource}
-        renderRow={this.renderRow}
+      <FlatList
+        data={this.data}
+        renderItem={this.renderItem}
       />
     );
   }
