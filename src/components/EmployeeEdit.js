16e5ccaffcda70eca090574cc725c627f40732e7
Nathan Marley <nathan.marley@gmail.com>
2018-05-18 19:06:46 -0700

    add confirm modal

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeEdit extends Component {
+  state = { showModal: false };
+
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
@@ -41,6 +43,16 @@ class EmployeeEdit extends Component {
             Text Schedule
           </Button>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+            Fire Employee
+          </Button>
+        </CardSection>
+
+        <Confirm>
+          Are you sure you want to delete this?
+        </Confirm>
       </Card>
     );
   }
