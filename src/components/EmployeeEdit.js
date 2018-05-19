53f1df658668a4febf893b6a1fdcb674b90661e9
Nathan Marley <nathan.marley@gmail.com>
2018-05-18 20:32:13 -0700

    add employee deletes

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
-import { employeeUpdate, employeeSave } from '../actions';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
 import { Card, CardSection, Button, Confirm } from './common';
 import EmployeeForm from './EmployeeForm';
 
@@ -25,6 +25,15 @@ class EmployeeEdit extends Component {
     const { phone, shift } = this.props;
 
     Communications.text(phone, `You upcoming shift is on ${shift}`);
+  }
+
+  onAccept() {
+    const { uid } = this.props.employee;
+    this.props.employeeDelete({ uid });
+  }
+
+  onDecline() {
+    this.setState({ showModal: false });
   }
 
   render() {
@@ -50,7 +59,11 @@ class EmployeeEdit extends Component {
           </Button>
         </CardSection>
 
-        <Confirm>
+        <Confirm
+          visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
+        >
           Are you sure you want to delete this?
         </Confirm>
       </Card>
@@ -66,4 +79,5 @@ const mapStateToProps = (state) => {
 export default connect(mapStateToProps, {
   employeeUpdate,
   employeeSave,
+  employeeDelete,
 })(EmployeeEdit);
